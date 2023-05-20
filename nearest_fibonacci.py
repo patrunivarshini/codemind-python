ebac110fdb065fde2b7d593d41155f4e3eeee55e
patrunivarshini <119914370+patrunivarshini@users.noreply.github.com>
2023-05-20 19:44:13 +0530

    def first(n,a,b):     while(n>=b):         c=a+b         a=b         b=c     return b def last(n,a,b):     while(n>=b):         c=a+b         a=b         b=c     return a n=int(input()) m=first(n,0,1) s=last(n,0,1) if (m-n>n-s):     print(s) elif(m-n<n-s):     print(m) else:     print(s,m,sep=" ")

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -4,16 +4,16 @@ def first(n,a,b):
         a=b
         b=c
     return b
-def lst(n,a,b):
+def last(n,a,b):
     while(n>=b):
         c=a+b
         a=b
         b=c
     return a
-n= int(input())
-m= first(n,0,1)
-s=lst(n,0,1)
-if(m-n>n-s):
+n=int(input())
+m=first(n,0,1)
+s=last(n,0,1)
+if (m-n>n-s):
     print(s)
 elif(m-n<n-s):
     print(m)
